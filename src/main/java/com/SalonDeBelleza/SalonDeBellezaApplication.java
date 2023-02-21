5407fb0cbc20e736471c86bb988e63d1b9d7c719
Asus <Asus@Vargas>
2023-02-21 00:12:07 -0600

    Inicialización del Proyecto FInal Asignación de puerto Cración de folders para el front-end

---
+++ src/main/java/com/SalonDeBelleza/SalonDeBellezaApplication.java
@@ -0,0 +1,13 @@
+package com.SalonDeBelleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonDeBellezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonDeBellezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SalonDeBelleza/SalonDeBellezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SalonDeBelleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonDeBellezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
